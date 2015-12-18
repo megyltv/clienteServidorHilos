5964af5ecbcf043b837bf73190453c8f07ae5e96
megyltv <megyltv@hotmail.com>
2015-12-18 10:39:18 -0500

    con dos sockets

--- EchoServer.java
+++ EchoServer.java
@@ -1,28 +1,41 @@
-package echo;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package echoserver;
 
 import java.io.IOException;
 import java.net.*;
-import java.io.*;
+import java.util.ArrayList;
+//import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class EchoServer {
-    public static void main() throws IOException {
-        
+    public static void main(String[] args) throws IOException {   
         
         /*if (args.length != 1) {
             System.err.println("Usage: java EchoServer <port number>");
             System.exit(1);
         }*/
         
-        int portNumber = 8080;
+        int portNumber = 8000;
         
-        try         
+        try        
         {
-            ServerSocket serverSocket = new ServerSocket();
+            ServerSocket serverSocket = new ServerSocket(8000);
+            /*ArrayList<Socket> clientS=new ArrayList();
+            clientS.add(serverSocket.accept());
+            for(Socket s:clientS){
+                
+            }*/
             Socket clientSocket = serverSocket.accept();
-            Thread t1 = new Thread(new MiThread(clientSocket));
+            Socket clientSocket2 = serverSocket.accept();
+            Thread t1 = new Thread(new MiThread(clientSocket,clientSocket2));
+            Thread t2 = new Thread(new MiThread(clientSocket2,clientSocket));
             t1.start();
+            t2.start();
             
             try {
                 t1.join();
--- MiThread.java
+++ MiThread.java
@@ -3,8 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package echo;
-
+package echoserver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -13,25 +12,25 @@ import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author USRDES
- */
-public class MiThread extends Thread {
-    
+public class MiThread extends Thread{
+   
     Socket clientSocket;
-    public MiThread(Socket Socket){
-        clientSocket=Socket;
+    Socket clientSocket2;
+    
+    public MiThread(Socket client,Socket client2){
+        clientSocket=client;
+        clientSocket2=client2;
     }
     
     public void run(){
-        PrintWriter out = null;                   
+        PrintWriter out = null;   
         try {
             out = new PrintWriter(clientSocket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(
-                    new InputStreamReader(clientSocket.getInputStream()));
+                    new InputStreamReader(clientSocket2.getInputStream()));
             String inputLine;
             while ((inputLine = in.readLine()) != null) {
+                System.out.println(inputLine);
                 out.println(inputLine);
             }
         } catch (IOException ex) {
